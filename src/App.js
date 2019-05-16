52a619d083186ca33638af8e87003b747e211eff
ANDRIFRITZENT <andryfritzent05@gmail.com>
2019-05-16 13:37:10 +0700

    satu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Team from './pages/Team';
+import Contact from './pages/Contact';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      name : "Home",
+    };
+  }
+  changeHandler(event) {
+    this.setState({
+      [event.target.name]: event.target.value 
+    });
+  }
+  clicked(menu){
+    this.setState({
+      name : menu.name,
+    });
+  }
+  info(name){    
+    if(name === 'Profile'){
+      return (
+        <Profile/>
+      );
+    } else if(name === 'Team'){
+      return (
+        <Team/>
+      );
+    } else if(name === 'Contact'){
+      return (
+        <Contact/>
+      );
+    } else {
+      //display default
+      return (
+        <Home/>
+      );
+    }
+  }
+  render() { 
+    return ( 
+      <React.Fragment>
+        <div class="mainmenu-area" data-spy="affix" data-offset-top="100">
+          <div class="container">
+              <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#primary-menu">
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand logo">
+                      <h2>Websiteku</h2>
+                  </a>
+              </div>
+              <nav className="collapse navbar-collapse" id="primary-menu">
+                <ul className="nav navbar-nav navbar-right">{
+                  this.props.pages.map((menu,index) => {
+                    let style = '';
+                    if(this.state.name === menu.name){
+                      style = `active`;
+                    }
+                    return <li                      
+                      className = { style }
+                      onClick = {this.clicked.bind(this,menu)} 
+                      key = {index}
+                    ><a>
+                      {menu.name}
+                    </a></li>;
+                  }
+                )}
+              </ul>
+            </nav>
+          </div>
+        </div>        
+
+        <div>
+            {this.info(this.state.name)}
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const pages = [
+    {
+      name : 'Home',
+    },    
+    {
+      name : 'Profile',
+    },
+    {
+      name : 'Team',
+    },
+    {
+      name : 'Contact',
+    },
+  ];
+
+ReactDOM.render(<App pages={pages}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
